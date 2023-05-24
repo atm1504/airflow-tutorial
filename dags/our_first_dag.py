de9c0be9bd4500e2e02b11a0e982c3a6222b5bc3
Amartya Mondal <atm1504.in@gmail.com>
2023-05-24 14:24:52 +0530

    Different dag version

--- dags/our_first_dag.py
+++ dags/our_first_dag.py
@@ -9,7 +9,7 @@ default_args={
 }
 
 with DAG(
-    dag_id='our_first_dag',
+    dag_id='our_first_dag_v4',
     default_args=default_args,
     description='This is my first dag',
     start_date=datetime(2023, 5,24 ),
@@ -19,4 +19,15 @@ with DAG(
         task_id='first_task',
         bash_command='echo hello world, this is gadhera hain tu!!'
     )
-    task1
+    task2=BashOperator(
+        task_id='second_task',
+        bash_command="echo u r looking very beautiful! April fool"
+    )
+    task3=BashOperator(
+        task_id='third_task',
+        bash_command="echo Hain thik hai makeup karle be..."
+    )
+    # task1.set_downstream(task2)
+    # task1.set_downstream(task3)
+    task1>>task2
+    task1>>task3
