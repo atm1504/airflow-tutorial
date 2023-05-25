62c245d808cd35e316940e7cb8d3b3fdd6705893
Amartya Mondal <atm1504.in@gmail.com>
2023-05-25 15:52:42 +0530

    Another version

--- dags/dag_with_taskflow_api.py
+++ dags/dag_with_taskflow_api.py
@@ -10,7 +10,7 @@ default_args = {
 
 
 @dag(
-    dag_id='our_dag__with_python_taskflow_v1',
+    dag_id='our_dag__with_python_taskflow_v2',
     default_args=default_args,
     description='dag using decorators',
     start_date=datetime(2023, 5, 24),
@@ -18,21 +18,30 @@ default_args = {
 )
 def hello_world_etl():
 
-    @task()
+    # @task()
+    # def get_name():
+    #     return "Dusht"
+
+    @task(multiple_outputs=True)
     def get_name():
-        return "Dusht"
+        return {
+            "first_name": "Jerry",
+            "last_name": 'Tom'
+        }
 
     @task()
     def get_age():
         return 19
 
     @task()
-    def greet(name, age):
-        print(f"Kya re {name}, kinti badi ho gayi re tu {age} saal ki hogayi")
+    def greet(first_name, last_name, age):
+        print(
+            f"Kya re {first_name} {last_name}, kinti badi ho gayi re tu {age} saal ki hogayi")
 
-    name = get_name()
+    name_dict = get_name()
     age = get_age()
-    greet(name=name, age=age)
+    greet(first_name=name_dict['first_name'],
+          last_name=name_dict['last_name'], age=age)
 
 
 greet_dag = hello_world_etl()
