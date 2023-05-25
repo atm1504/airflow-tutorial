d284011d5cf8c1393307390cc9d543c99911647f
Amartya Mondal <atm1504.in@gmail.com>
2023-05-25 15:32:16 +0530

    6th version

--- dags/create_dag_with_operator.py
+++ dags/create_dag_with_operator.py
@@ -9,32 +9,44 @@ default_args = {
 }
 
 
-def greet(name, age):
-    print("Hello devyani tu kitni pagal hain!")
-    print(f"Kya re {name}, kinti badi ho gayi re tu {age} saal ki hogayi")
+def greet(ti):
+    first_name = ti.xcom_pull(task_ids='get_name', key='first_name')
+    last_name = ti.xcom_pull(task_ids='get_name', key='last_name')
+    age = ti.xcom_pull(task_ids='get_age', key='age')
+    print(
+        f"Kya re {first_name} {last_name}, kinti badi ho gayi re tu {age} saal ki hogayi")
 
 
-def get_name():
-    return "atm"
+def get_name(ti):
+    ti.xcom_push(key='first_name', value='Jerry')
+    ti.xcom_push(key='last_name', value='Friedman')
+
+
+def get_age(ti):
+    ti.xcom_push(key='age', value=40)
 
 
 with DAG(
-    dag_id='our_dag__with_python_operator_v3',
+    dag_id='our_dag__with_python_operator_v6',
     default_args=default_args,
     description='dag using python operator',
     start_date=datetime(2023, 5, 24),
     schedule_interval='@daily'
 ) as dag:
-    # task1 = PythonOperator(
-    #     task_id='first_task',
-    #     python_callable=greet,
-    #     op_kwargs={
-    #         'name': 'Devyani',
-    #         'age': 22
-    #     }
-    # )
+    task1 = PythonOperator(
+        task_id='greet',
+        python_callable=greet,
+        # op_kwargs={
+        #     'age': 22
+        # }
+    )
     task2 = PythonOperator(
         task_id='get_name',
         python_callable=get_name
     )
-    task2
+
+    task3 = PythonOperator(
+        task_id='get_age',
+        python_callable=get_age
+    )
+    [task2, task3] >> task1
